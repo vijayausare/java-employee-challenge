39db948ce61f72ffb508a5e7e3141cc2e5107cb5
vijayausare <vijayausare0007@gmail.com>
2025-10-04 03:29:08 +0530

    Model: Added Employee class

---
+++ api/src/main/java/com/reliaquest/api/model/Employee.java
@@ -0,0 +1,12 @@
+package com.reliaquest.api.model;
+
+import java.util.UUID;
+
+public class Employee {
+    private UUID id;
+    private String name;
+    private Integer salary;
+    private Integer age;
+    private String title;
+    private String email;
+}
